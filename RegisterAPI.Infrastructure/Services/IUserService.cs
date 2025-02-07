ee8bc7904e192bb2713b314a2a99053638a4351f
Bhupendra Jaiswal <bhupintpc@gmail.com>
2025-02-07 12:58:51 +1100

    initial commit REGister API

---
+++ RegisterAPI.Infrastructure/Services/IUserService.cs
@@ -0,0 +1,12 @@
+﻿using RegisterAPI.Infrastructure.Data.Dto;
+
+namespace RegisterAPI.Infrastructure.Services
+{
+    public interface IUserService
+    {
+        Task<IEnumerable<UserDto>> GetAllUsersAsync();
+        Task<bool> RegisterUserAsync(UserRegistrationDto userDto);
+        Task<UserDto> GetUser();
+    }
+
+}
